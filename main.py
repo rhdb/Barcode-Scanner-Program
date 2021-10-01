1fca8faecdc6deaca06a0b334788f8a20eb26ed1
A. Teo Welton <76081718+DragonDev07@users.noreply.github.com>
2021-10-01 09:48:52 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -30,34 +30,32 @@ sheet = client.open(sheet_name).sheet1
 data = sheet.get_all_records()
 
 # Find the student based on their ID:
-student_id = 54527 # Milo Banks
+student_id = int(input("Student ID: "))
 
 #Scanned Before?
 trackingTime=False
-
+index = 0
 
 for entry in data:
   if entry['Person ID'] == student_id:
-    if trackingTime==True:
-      index = data.index(entry)
-      break
+    #if trackingTime==True:
+    index = data.index(entry)
+    break
     
-    elif trackingTime==False:
-      trackingTime=True
-      startingTime=datetime.datetime.now()
-      startingTime="{}".format(startingTime)
-      startingHour1= startingTime[10:13]
-      startingMin1 = startingTime[14:16]
-      startingHour1=int(startingHour1)-6
+    #elif not trackingTime:
+     # trackingTime=True
+     # startingTime=datetime.datetime.now()
+     # startingTime="{}".format(startingTime)
+     # startingHour1= startingTime[10:13]
+     # startingMin1 = startingTime[14:16]
+     # startingHour1=int(startingHour1)-6
 
     # There should never be an instance in which the student ID isn't found because we are scraping the google sheet and there isn't a way for a student ID to be entered incorrectly.
-
+  
 index += 2
-
-# Update the student's total hours:
+  # Update the student's total hours:
 
 workout_hours = 1 # Change this value accordingly.
 total_hours = sheet.cell(index,4).value 
 
 sheet.update_cell(index, 4, int(total_hours) + workout_hours)
-trackingTime= False
